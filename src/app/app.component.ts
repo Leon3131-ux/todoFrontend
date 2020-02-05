bf31e956e664b4a0981a803c55b23244e92f5404
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-02-05 15:30:51 +0100

    angProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {TaskDto} from './classes/task-dto';
+import {TaskService} from './services/task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges {
+  constructor(private taskService: TaskService) { }
   title = 'angProject';
+  currentEntry: TaskDto = null;
+  currentEntryId: number = null;
+  tasks: TaskDto[];
+  @Input() savedTask: TaskDto;
+
+  ngOnInit(): void {
+    this.taskService.findAll().subscribe(data => {
+      this.tasks = data;
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.savedTask.currentValue) {
+      console.log(this.savedTask);
+    }
+  }
+  onSelectedEntry(selectedEntry: TaskDto) {
+    this.currentEntry = selectedEntry;
+    this.currentEntryId = selectedEntry.id;
+  }
+  onSavedTask(savedTask: TaskDto) {
+    if (this.currentEntry === null) {
+      this.tasks.push(savedTask);
+    } else {
+      this.currentEntry = Object.assign(this.currentEntry, savedTask);
+    }
+  }
+  onFormReset() {
+    this.resetValues();
+  }
+  onDeleteTask() {
+    const index = this.tasks.indexOf(this.currentEntry);
+    this.tasks.splice(index, 1);
+    this.resetValues();
+  }
+  resetValues() {
+    this.currentEntry = null;
+    this.currentEntryId = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { MultiResultComponent } from './search-result/multi-result/multi-result.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TaskTableComponent } from './task-table/task-table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TaskService } from './services/task.service';
+import { TaskSaveComponent } from './task-save/task-save.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiResultComponent,
+    NavbarComponent,
+    TaskTableComponent,
+    TaskSaveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
