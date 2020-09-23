22421ca9184a75b11d5b52175fb965c69476bdb1
leon3131-ux <56247087+Leon3131-ux@users.noreply.github.com>
2020-09-23 10:01:27 +0200

    renamed project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngProject
+# TODOApplicationAngularFrontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.23.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angProject app is running!');
+    expect(page.getTitleText()).toEqual('TODOApplicationAngularFrontend app is running!');
   });
 
   afterEach(async () => {
